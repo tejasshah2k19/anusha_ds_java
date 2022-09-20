de3835a75e90b9ab5288ac9b086347242aa0f5b9
tejas <tejasshah@gmail.com>
2022-09-20 23:01:21 +0530

    kruskal done

--- src/graph/GraphLL.java
+++ src/graph/GraphLL.java
@@ -1,9 +1,11 @@
 package graph;
 
+import java.util.HashSet;
+
 public class GraphLL {
 
 	public static void main(String[] args) {
-		Graph g = new Graph(4, 4);// 4 vertices and 4 edges
+		Graph g = new Graph(5, 6);// 4 vertices and 4 edges
 
 		g.edges[0].addEdge(0, 1, 2);
 		g.edges[1].addEdge(1, 0, 2);
@@ -17,14 +19,22 @@ public class GraphLL {
 		g.edges[6].addEdge(2, 0, 2);
 		g.edges[7].addEdge(0, 2, 2);
 
+		g.edges[8].addEdge(2, 4,1);
+		g.edges[9].addEdge(4, 2, 1);
+
+		
+		g.edges[10].addEdge(3, 4, 1);
+		g.edges[11].addEdge(4, 3, 1);
+
+		
 		g.printGraph();
 		System.out.println("Edges ==> ");
 		g.printEdges();
 		g.sortEdges();
 		System.out.println("Edges===> ");
 		g.printEdges();
-		
-		g.kruskal(); 
+
+		g.kruskal();
 	}
 }
 
@@ -78,38 +88,51 @@ class Graph {
 		System.out.println("********KRUSKAL************");
 		int count = 0;
 		int i = 0;
-		int cost =0;
+		int cost = 0;
 
 		while (count != this.V - 1) {// 3
-			if (this.edges[i].visited == false  &&  intersection(this.edges[i].src,this.edges[i].dest) == false  ) {
-				this.edges[i].visited = true; // 3 2 => true 
+			if (this.edges[i].visited == false && intersection(this.edges[i].src, this.edges[i].dest) == false) {
+				this.edges[i].visited = true; // 3 2 => true
 				setVisited(this.edges[i].dest, this.edges[i].src, true);
-				count++;// 1  2 
-				System.out.println(this.edges[i].src+"-"+this.edges[i].dest);//3-0  , 
-				cost = cost+ this.edges[i].weight; 
+				count++;// 1 2
+				System.out.println(this.edges[i].src + "-" + this.edges[i].dest);// 3-0 ,
+				cost = cost + this.edges[i].weight;
 			}
 
-			i++;// 0 1 2 3 
+			i++;// 0 1 2 3
 		}
-		System.out.println("cost => "+cost);
+		System.out.println("cost => " + cost);
 		System.out.println("************END**************");
 	}
 
 	void setVisited(int src, int dest, boolean status) {
 		for (int i = 0; i < this.E; i++) {
 			if (this.edges[i].src == src && this.edges[i].dest == dest) {
-				this.edges[i].visited = status; // true 
+				this.edges[i].visited = status; // true
 				break;
 			}
 		}
 	}
-	
-	boolean intersection(int src,int dest) {
-		//set => src => 
-		//set => dest => 
-		//intersect => mila --> true 
-		// not found => false 
-		return false;
+
+	boolean intersection(int x, int y) {
+		HashSet<Integer> xSet = new HashSet<Integer>();
+		HashSet<Integer> ySet = new HashSet<Integer>();
+
+		for (int i = 0; i < this.E; i++) {
+			if (this.edges[i].visited == true && this.edges[i].src == x) {
+				xSet.add(this.edges[i].dest);
+			}
+			if (this.edges[i].visited == true && this.edges[i].src == y) {
+				ySet.add(this.edges[i].dest);
+			}
+
+		}
+		xSet.retainAll(ySet);
+
+		if (xSet.size() == 0) {
+			return false;
+		}
+		return true;
 	}
 }
 
